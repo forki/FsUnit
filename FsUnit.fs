7623fc13439f0e60bd05c1ed3b5f6dcb937fe468
Steffen Forkmann <sforkmann@gmail.com>
2014-09-17 11:12:26 +0200

    Update FsUnit.fs

--- FsUnit.fs
+++ FsUnit.fs
@@ -1,9 +1,16 @@
 namespace FsUnit
 
+open System.Diagnostics
 open NUnit.Framework
 
 [<AutoOpen>]
 module Extensions =
+
+  [<DebuggerStepThrough>]
   let shouldEqual (x: 'a) (y: 'a) = Assert.AreEqual(x, y, sprintf "Expected: %A\nActual: %A" x y)
+
+  [<DebuggerStepThrough>]
   let shouldBeSmallerThan (x: 'a) (y: 'a) = Assert.GreaterOrEqual(x, y, sprintf "Expected: %A\nActual: %A" x y)
+
+  [<DebuggerStepThrough>]
   let shouldBeGreaterThan (x: 'a) (y: 'a) = Assert.Greater(y, x, sprintf "Expected: %A\nActual: %A" x y)
