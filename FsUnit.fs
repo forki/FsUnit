22b3653a865ec4482f195e71b4b1ca328b562c20
Steffen Forkmann <sforkmann@gmail.com>
2014-10-01 17:01:23 +0200

    shouldNotContain

--- FsUnit.fs
+++ FsUnit.fs
@@ -5,20 +5,23 @@ open NUnit.Framework
 open System.Collections.Generic
 
 [<AutoOpen>]
-module Extensions =
-
-  [<DebuggerStepThrough>]
-  let shouldEqual (x: 'a) (y: 'a) = Assert.AreEqual(x, y, sprintf "Expected: %A\nActual: %A" x y)
-
-  [<DebuggerStepThrough>]
-  let shouldContain (x: 'a) (y: 'a seq) =
-    let list = List<_>()
-    for a in y do
-        list.Add a
-    Assert.Contains(x, list)
-
-  [<DebuggerStepThrough>]
-  let shouldBeSmallerThan (x: 'a) (y: 'a) = Assert.GreaterOrEqual(x, y, sprintf "Expected: %A\nActual: %A" x y)
-
-  [<DebuggerStepThrough>]
-  let shouldBeGreaterThan (x: 'a) (y: 'a) = Assert.Greater(y, x, sprintf "Expected: %A\nActual: %A" x y)
+module Extensions = 
+    [<DebuggerStepThrough>]
+    let shouldEqual (x : 'a) (y : 'a) = Assert.AreEqual(x, y, sprintf "Expected: %A\nActual: %A" x y)
+    
+    [<DebuggerStepThrough>]
+    let shouldContain (x : 'a) (y : 'a seq) = 
+        let list = List<_>()
+        for a in y do
+            list.Add a
+        Assert.Contains(x, list)
+    
+    [<DebuggerStepThrough>]
+    let shouldNotContain (x : 'a) (y : 'a seq) = 
+        if Seq.exists ((=) x) y then failwithf "Seq %A should not contain %A" y x
+    
+    [<DebuggerStepThrough>]
+    let shouldBeSmallerThan (x : 'a) (y : 'a) = Assert.GreaterOrEqual(x, y, sprintf "Expected: %A\nActual: %A" x y)
+    
+    [<DebuggerStepThrough>]
+    let shouldBeGreaterThan (x : 'a) (y : 'a) = Assert.Greater(y, x, sprintf "Expected: %A\nActual: %A" x y)
